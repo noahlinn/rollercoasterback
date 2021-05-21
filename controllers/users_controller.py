bca2edcbab0ecb783754a170b8a9cbd9d3daa884
noahlinn <n.linn95@gmail.com>
2021-05-21 16:32:26 -0400

    basic user routes done

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+
 def create_user():
     existing_user = models.User.query.filter_by(email = request.json["email"]).first()
     if existing_user:
@@ -22,3 +23,21 @@ def create_user():
     encrypted_id = jwt.encode({ "user_id": user.id }, os.environ.get('JWT_SECRET'), algorithm="HS256")
 
     return{"user": user.to_json(), "user_id": encrypted_id}
+
+def login():
+    user = models.User.query.filter_by(email=request.json["email"]).first()
+    if not user:
+        return{"message": "User not found"}, 404
+    if bcrypt.check_password_hash(user.password, request.json["password"]):
+        encrypted_id = jwt.encode({"user_id": user.id}, os.environ.get('JWT_SECRET'), algorithm = "HS256")
+        return {"user": user.to_json(), "user_id": encrypted_id}
+    else:
+        return{"message": "Password incorrect"}, 401
+
+def verify():
+    decrypted_id = jwt.decode(request.headers["Authorization"], os.environ.get('JWT_SECRET'), algorithms=["HS256"])["user_id"]
+    user = models.User.query.filter_by(id=decrypted_id).first()
+    if not user:
+        return{"message": "user not found"},404
+    return{"user": user.to_json()}
+    
--- routes/user_routes.py
+++ routes/user_routes.py
@@ -2,3 +2,5 @@ from controllers import users_controller
 
 def apply_users_routes(app):
     app.route('/users', methods=["POST"])(users_controller.create_user)
+    app.route('/users/login', methods=["POST"])(users_controller.login)
+    app.route('/users/verify', methods=["GET"])(users_controller.verify)
