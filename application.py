b39289f9aff9ef7d5576f859ba8688e009d65e1d
noahlinn <n.linn95@gmail.com>
2021-05-26 15:50:36 -0400

    test

--- application.py
+++ application.py
@@ -17,7 +17,7 @@ def root():
 app.route('/', methods=["GET"])(root)
 
 apply_routes(app)
-
+# test
 if __name__ == '__main__':
   port = int(os.environ.get('PORT')) or 5000
   app.run('0.0.0.0', port=port, debug=True)
